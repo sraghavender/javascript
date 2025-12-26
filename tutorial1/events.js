e3a09ba7d5540a500b138f64e2fde20ca020e5f2
Raghavender <s.raghavender@gmail.com>
2025-12-26 19:40:24 +0530

    on click javascript example

---
+++ tutorial1/events.js
@@ -0,0 +1,6 @@
+function doSomething(){
+    console.log('Clicked the square!');
+    document.getElementById('block').style.backgroundColor='crimson';
+    document.getElementById('block').style.borderRadius='100%';
+
+}
