0712e6502aab3605dd57302165d946bf6d0157cd
Raghavender <s.raghavender@gmail.com>
2025-12-27 11:09:11 +0530

    lesson3

---
+++ tutorial/lesson3/events.js
@@ -0,0 +1,29 @@
+// basic data types in java script
+
+var text = "This is text"; // String variable
+var number=121; // Number varaiable
+var isBoolean=true; //Boolean
+var nothing=null; // Null variable
+var hello; // undefined
+var array =["one","two","three"];       // Array of strings
+var object = {name:"Mary",age:34}; // Object
+
+// log all variable types to console
+
+console.log(text);
+console.log(number);
+console.log(isBoolean);
+console.log(nothing)
+console.log(hello)
+console.log(array)
+console.log(object) 
+
+// type casting
+
+console.log(121+Number("20"));
+Number(text);   
+console.log(typeof(text));
+console.log(number)
+console.log(typeof String(number))
+String(number)
+console.log(typeof(number))
