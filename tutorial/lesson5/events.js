d2e716cac19a1d85a0b45c464f802c9cf052ded4
Raghavender <s.raghavender@gmail.com>
2026-01-05 20:28:33 +0530

    if else statement

---
+++ tutorial/lesson5/events.js
@@ -0,0 +1,13 @@
+var text = "How are you"
+
+if(text == "Hello")
+    console.log("Hello");
+else if (text == "How are you")
+    console.log("Good");
+else
+console.log("Good bye!")
+
+var age =15;
+
+var drink = (age<18) ? "Juice" : "Beer";
+console.log(drink);
