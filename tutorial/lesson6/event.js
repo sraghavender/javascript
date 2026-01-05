738fc4cf93888dcc4bdae19e92049ba7816de06a
Raghavender <s.raghavender@gmail.com>
2026-01-05 20:49:09 +0530

    for loop example

---
+++ tutorial/lesson6/event.js
@@ -0,0 +1,12 @@
+console.log("Printing numbers till 10");
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+console.log("determine odd or even numbers")
+
+for(let j=0;j<10;j++){
+var value = (j%2==0) ? "Even number:" +j : "Odd number:" + j;
+console.log(value);
+
+}
